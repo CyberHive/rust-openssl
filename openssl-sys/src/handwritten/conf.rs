cd93b36523311b27ebb856e8db602f36b0b0063c
Stuart Hickey <Stuart.Hickey@100percentit.com>
2023-08-14 11:34:55 +0100

    ffi bindings

--- openssl-sys/src/handwritten/conf.rs
+++ openssl-sys/src/handwritten/conf.rs
@@ -4,4 +4,9 @@ extern "C" {
     pub fn NCONF_new(meth: *mut CONF_METHOD) -> *mut CONF;
     pub fn NCONF_default() -> *mut CONF_METHOD;
     pub fn NCONF_free(conf: *mut CONF);
+    pub fn CONF_modules_load_file(
+        filename: *const c_char,
+        appname: *const c_char,
+        flags: c_ulong,
+    ) -> c_int;
 }
